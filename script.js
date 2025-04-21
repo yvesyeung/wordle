b5bbf0f3b9ff3a5463f3d43d7853db4e457d1bb6
yvesyeung <18182638+yvesyeung@users.noreply.github.com>
2025-04-20 21:49:16 -0700

    Created

---
+++ script.js
@@ -0,0 +1,77 @@
+const word = "hello"; // Word to guess
+let currRow = 0;
+let currTile = 0;
+let row = document.getElementsByClassName("row")[currRow];
+let tiles = row.getElementsByClassName("tile");
+
+// Handle
+const submitGuess = function () {
+  // Word not long enough
+  if (currTile < 5) {
+    console.log("Not enough letters!");
+    return;
+  }
+
+  // TODO: Handle invalid word
+
+  let tempWord = word.split("");
+  let status = Array(5).fill("absent");
+  let correctLetters = 0;
+
+  // Identify any correct letters
+  for (let i = 0; i < 5; i++) {
+    if (tiles[i].textContent == word[i]) {
+      status[i] = "correct";
+      tempWord[i] = null; // Mark letter as used
+      correctLetters++;
+    }
+  }
+
+  // Identify any present characters
+  for (let i = 0; i < 5; i++) {
+    if (status[i] == "correct") continue;
+    const index = tempWord.indexOf(tiles[i].textContent);
+    if (index !== -1) {
+      status[i] = "present";
+      tempWord[index] = null;
+    }
+  }
+
+  // Update UI
+  for (let i = 0; i < 5; i++) {
+    tiles[i].classList.add(status[i]);
+  }
+
+  // Player wins
+  if (correctLetters == 5) {
+    console.log("You win!");
+    return;
+  }
+
+  // Player loses
+  if (currRow == 5) {
+    console.log("You lose");
+    return;
+  }
+
+  // Go to next row
+  if (currRow < 5) {
+    currRow++;
+    currTile = 0;
+    row = document.getElementsByClassName("row")[currRow];
+    tiles = row.getElementsByClassName("tile");
+  }
+};
+
+// Listen to key presses and take appropriate action
+document.addEventListener("keydown", function (event) {
+  if (/^[a-zA-Z]$/.test(event.key) && currTile < 5) {
+    tiles[currTile].textContent = event.key;
+    currTile++;
+  } else if (event.key == "Backspace" && currTile > 0) {
+    currTile--;
+    tiles[currTile].textContent = "";
+  } else if (event.key == "Enter") {
+    submitGuess();
+  }
+});
